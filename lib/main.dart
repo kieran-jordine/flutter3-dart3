00f3010752218357ea05a8c603b9498931095ef3
kjordine <kieranjordine@gmail.com>
2023-07-28 06:32:46 -0500

    upgrade to platform 34

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
+      debugShowCheckedModeBanner: false,
       routes: namedRoutes,
       // initialRoute: '/',
       onGenerateRoute: generatedRoute,
